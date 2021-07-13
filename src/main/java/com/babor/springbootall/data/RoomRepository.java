2b26e1a00d0b578f83d4c08629d28450ccbc14f8
Shofiullah Babor <babu_12f@yahoo.com>
2021-07-13 21:50:40 +0600

    sample embaded database

---
+++ src/main/java/com/babor/springbootall/data/RoomRepository.java
@@ -0,0 +1,9 @@
+package com.babor.springbootall.data;
+
+import com.babor.springbootall.models.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoomRepository extends JpaRepository<Room, Long> {
+}
