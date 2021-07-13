42a72c3f8c42804302b7d2e6509725cde7682952
Shofiullah Babor <babu_12f@yahoo.com>
2021-07-13 20:06:38 +0600

    first task

---
+++ src/main/java/com/babor/springbootall/service/StaffService.java
@@ -0,0 +1,27 @@
+package com.babor.springbootall.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+
+import com.babor.springbootall.models.Position;
+import com.babor.springbootall.models.StaffMember;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StaffService {
+
+    private static final List<StaffMember> staff = new ArrayList();
+
+    static{
+        staff.add(new StaffMember(UUID.randomUUID().toString(), "John", "Doe", Position.CONCIERGE));
+        staff.add(new StaffMember(UUID.randomUUID().toString(), "Jane", "Doe", Position.FRONT_DESK));
+        staff.add(new StaffMember(UUID.randomUUID().toString(), "Oliver", "Handle", Position.SECURITY));
+        staff.add(new StaffMember(UUID.randomUUID().toString(), "Sammy", "Smith", Position.HOUSEKEEPING));
+    }
+
+    public List<StaffMember> getAllStaff(){
+        return staff;
+    }
+}
