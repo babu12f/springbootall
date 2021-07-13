18439f901c41e1e9b970a441c71b059cd91c2ce0
Shofiullah Babor <babu_12f@yahoo.com>
2021-07-13 20:22:11 +0600

    tast 2 rest controller

---
+++ src/main/java/com/babor/springbootall/controllers/RoomRestController.java
@@ -0,0 +1,25 @@
+package com.babor.springbootall.controllers;
+
+import com.babor.springbootall.models.Room;
+import com.babor.springbootall.service.RoomService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/rooms")
+public class RoomRestController {
+
+    private final RoomService roomService;
+
+    public RoomRestController(RoomService roomService) {
+        this.roomService = roomService;
+    }
+
+    @GetMapping
+    public List<Room> getAllRoom() {
+        return roomService.getAllRooms();
+    }
+}
