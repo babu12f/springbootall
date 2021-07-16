c4992bf7abb87f51edfe35e317e6ddf38425f311
Shofiullah Babor <babu_12f@yahoo.com>
2021-07-16 10:57:58 +0600

    spring rest repository

--- src/main/java/com/babor/springbootall/config/SecurityConfig.java
+++ src/main/java/com/babor/springbootall/config/SecurityConfig.java
@@ -17,7 +17,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/api/*").permitAll()
+                .antMatchers("/", "/api/*", "/api/rest/*").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
