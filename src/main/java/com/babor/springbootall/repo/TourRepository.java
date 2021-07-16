12ddfc92d215ccedea25b5e6fcdc83a404bc5d9a
Shofiullah Babor <babu_12f@yahoo.com>
2021-07-17 02:39:42 +0600

    add paging ans sorting

--- src/main/java/com/babor/springbootall/repo/TourRepository.java
+++ src/main/java/com/babor/springbootall/repo/TourRepository.java
@@ -1,11 +1,11 @@
 package com.babor.springbootall.repo;
 
 import com.babor.springbootall.domain.Tour;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
-
-public interface TourRepository extends CrudRepository<Tour, Integer> {
-    List<Tour> findByTourPackageCode(@Param("code") String code);
+public interface TourRepository extends PagingAndSortingRepository<Tour, Integer> {
+    Page<Tour> findByTourPackageCode(@Param("code") String code, Pageable pageable);
 }
