a93e9a5c8ceec2ceac488cec33c857d20ede2100
Shofiullah Babor <babu_12f@yahoo.com>
2021-07-17 02:30:11 +0600

    add search functionality to default rest data

--- src/main/java/com/babor/springbootall/repo/TourRepository.java
+++ src/main/java/com/babor/springbootall/repo/TourRepository.java
@@ -2,6 +2,10 @@ package com.babor.springbootall.repo;
 
 import com.babor.springbootall.domain.Tour;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface TourRepository extends CrudRepository<Tour, Integer> {
+    List<Tour> findByTourPackageCode(@Param("code") String code);
 }
